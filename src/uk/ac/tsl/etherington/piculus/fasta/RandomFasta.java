8eb1a9b4e7623b44201646e42ab64932a38eaadd
ethering <Graham.Etherington@bbsrc.ac.uk>
2013-10-16 15:06:56 +0100

    Handle situation where number of seqs requested is greater than that available

--- src/uk/ac/tsl/etherington/piculus/fasta/RandomFasta.java
+++ src/uk/ac/tsl/etherington/piculus/fasta/RandomFasta.java
@@ -4,35 +4,14 @@
  */
 package uk.ac.tsl.etherington.piculus.fasta;
 
-import java.io.BufferedInputStream;
-import java.io.BufferedReader;
-import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
-import java.io.FileReader;
-import java.io.FileWriter;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.LineNumberReader;
 import java.util.ArrayList;
-import java.util.LinkedHashMap;
 import java.util.List;
-import java.util.Map;
 import mathsutils.RandomUtils;
-import org.biojava.bio.symbol.Alphabet;
-import org.biojava.bio.symbol.AlphabetManager;
-import org.biojava3.core.sequence.DNASequence;
-import org.biojava3.core.sequence.compound.AmbiguityDNACompoundSet;
-import org.biojava3.core.sequence.compound.NucleotideCompound;
-import org.biojava3.core.sequence.io.DNASequenceCreator;
-import org.biojava3.core.sequence.io.FastaReader;
-import org.biojava3.core.sequence.io.GenericFastaHeaderParser;
 import org.biojava3.data.sequence.FastaSequence;
 import org.biojava3.data.sequence.SequenceUtil;
-import org.biojavax.SimpleNamespace;
-import org.biojavax.bio.seq.RichSequence;
-import org.biojavax.bio.seq.RichSequenceIterator;
 
 /**
  *
@@ -42,8 +21,8 @@ public class RandomFasta
 {
     /**
      * 
-     * @param fasta
-     * @param numberOfSeqsRequired
+     * @param fasta the multi-fasta infile
+     * @param numberOfSeqsRequired the number of random sequences required
      * @param outfile
      * @throws Exception 
      */
@@ -51,6 +30,11 @@ public class RandomFasta
     {
         //read in the fasta sequences to a List
         List<FastaSequence> seqs = SequenceUtil.readFasta(new FileInputStream(fasta));
+        if (seqs.size() < numberOfSeqsRequired)
+        {
+            System.err.println("The number of random sequences requested is greater than that available.\nThere are "+seqs.size()+" sequences available");
+            System.exit(1);
+        }
         //get the random ints
         ArrayList<Integer> randomInts = RandomUtils.getRandomArray(numberOfSeqsRequired, seqs.size());
         //a holder
@@ -62,6 +46,7 @@ public class RandomFasta
         }
         FileOutputStream fop = new FileOutputStream(outfile);
         SequenceUtil.writeFasta(fop, dnaSeqs);
+        fop.close();
     }
         
    }

29b948f562a53a720d2ab39f3527e636baac0b5a
ethering <graham.etherington@tgac.ac.uk>
2016-02-01 16:11:18 +0000

    More code edits

--- src/uk/ac/tsl/etherington/genomehelper/fasta/FastaFeatures.java
+++ src/uk/ac/tsl/etherington/genomehelper/fasta/FastaFeatures.java
@@ -25,24 +25,30 @@ import org.biojavax.bio.seq.RichSequence;
 import org.biojavax.bio.seq.RichSequenceIterator;
 
 /**
- *A class of helper methods to obtain information and data structures from fasta files 
+ * A class of helper methods to obtain information and data structures from
+ * fasta files
+ *
  * @author ethering
  */
-public class FastaFeatures
+public
+        class FastaFeatures
 {
+
     /**
-     * 
+     *
      * @param refSeq a multi-fasta file of DNA sequences
-     * @return a HashMap of sequence names as keys with their sequence-lengths as values
+     * @return a HashMap of sequence names as keys with their sequence-lengths
+     * as values
      * @throws FileNotFoundException exception
      * @throws BioException exception
      */
-    public static HashMap<String, Integer> getSequenceLengths(File refSeq) throws FileNotFoundException, BioException, Exception
+    public static
+            HashMap<String, Integer> getSequenceLengths(File refSeq) throws FileNotFoundException, BioException, Exception
     {
 
         HashMap<String, Integer> seqLengths = new HashMap<>();
         LinkedHashMap<String, DNASequence> seqs = FastaReaderHelper.readFastaDNASequence(refSeq, true);
-        
+
         for (Map.Entry<String, DNASequence> entry : seqs.entrySet())
         {
             String seqName = entry.getKey();
@@ -51,15 +57,21 @@ public class FastaFeatures
         }
         return seqLengths;
     }
-    
+
     /**
-     * Parses a multi-fasta file into a LinkedHashMap of sequence names and DNASequences
+     * Parses a multi-fasta file into a LinkedHashMap of sequence names and
+     * DNASequences
+     *
      * @param refSeq a multi-fasta file of DNA sequences
-     * @return a LinkedHashMap of Accession numbers as keys with their DNA sequences as values. All sequence names will have any non-accession information stripped
-     * (e.g. "gi|2033910|gb|AA381581.1|AA381581 EST94688 Activated T-cells I Homo sapiens" would become "gi|2033910|gb|AA381581.1|AA381581")
-     * @throws Exception 
+     * @return a LinkedHashMap of Accession numbers as keys with their DNA
+     * sequences as values. All sequence names will have any non-accession
+     * information stripped (e.g. "gi|2033910|gb|AA381581.1|AA381581 EST94688
+     * Activated T-cells I Homo sapiens" would become
+     * "gi|2033910|gb|AA381581.1|AA381581")
+     * @throws Exception
      */
-    public static LinkedHashMap<String, DNASequence> getParsedDNASequences(File refSeq) throws Exception
+    public static
+            LinkedHashMap<String, DNASequence> getParsedDNASequences(File refSeq) throws Exception
     {
         LinkedHashMap<String, DNASequence> tempgenome = FastaReaderHelper.readFastaDNASequence(refSeq);
         LinkedHashMap<String, DNASequence> genome = new LinkedHashMap<>();
@@ -75,35 +87,37 @@ public class FastaFeatures
         tempgenome.clear();
         return genome;
     }
-    
+
     /**
-     * 
+     *
      * @param refSeq the reference sequence
      * @return the cumulative length of the provided sequences
      * @throws FileNotFoundException
-     * @throws BioException 
+     * @throws BioException
      */
-    
-    public static double getGenomeSize(File refSeq) throws FileNotFoundException, BioException, Exception
+    public static
+            double getGenomeSize(File refSeq) throws FileNotFoundException, BioException, Exception
     {
         double genomeSize = 0;
         HashMap<String, Integer> seqLengths = new HashMap<>(FastaFeatures.getSequenceLengths(refSeq));
         for (Map.Entry<String, Integer> entry : seqLengths.entrySet())
         {
             int genomeLength = entry.getValue();
-            genomeSize+=genomeLength;
+            genomeSize += genomeLength;
         }
         return genomeSize;
     }
 
     /**
-     * 
+     *
      * @param refSeq the reference sequence
-     * @return a HashMap where the keys are sequence names and the values are int arrays the length of the corresponding sequence, filed with zeros
+     * @return a HashMap where the keys are sequence names and the values are
+     * int arrays the length of the corresponding sequence, filed with zeros
      * @throws FileNotFoundException
-     * @throws BioException 
+     * @throws BioException
      */
-    public static HashMap getSequenceAsHashMapIntArray(File refSeq) throws FileNotFoundException, BioException, Exception
+    public static
+            HashMap getSequenceAsHashMapIntArray(File refSeq) throws FileNotFoundException, BioException, Exception
     {
 
         HashMap<String, Integer> seqLengths = new HashMap<>(FastaFeatures.getSequenceLengths(refSeq));
@@ -120,15 +134,16 @@ public class FastaFeatures
 
         return codingMap;
     }
-    
+
     /**
-     * 
+     *
      * @param refSeq the reference sequence
      * @return a sorted ArrayList of sequence lengths
      * @throws FileNotFoundException
-     * @throws BioException 
+     * @throws BioException
      */
-    public static ArrayList<Integer> getSequenceAsSortedIntArrayList(File refSeq) throws FileNotFoundException, BioException, Exception
+    public static
+            ArrayList<Integer> getSequenceAsSortedIntArrayList(File refSeq) throws FileNotFoundException, BioException, Exception
     {
 
         HashMap<String, Integer> seqLengths = new HashMap<>(FastaFeatures.getSequenceLengths(refSeq));
@@ -142,7 +157,9 @@ public class FastaFeatures
         Collections.reverse(lengths);
         return lengths;
     }
-    public static ArrayList<Integer> getSequenceAsSortedIntArrayList(File refSeq, int minContigSize) throws FileNotFoundException, BioException, Exception
+
+    public static
+            ArrayList<Integer> getSequenceAsSortedIntArrayList(File refSeq, int minContigSize) throws FileNotFoundException, BioException, Exception
     {
 
         HashMap<String, Integer> seqLengths = new HashMap<>(FastaFeatures.getSequenceLengths(refSeq));
@@ -155,72 +172,74 @@ public class FastaFeatures
             {
                 lengths.add(length);
             }
-            
+
         }
         Collections.sort(lengths);
         Collections.reverse(lengths);
         return lengths;
     }
-    
-    public void getNStats (ArrayList<Integer> sortedSeqLengths)
+
+    public
+            void getNStats(ArrayList<Integer> sortedSeqLengths)
     {
         double genomeSize = 0;
-        
+
         for (Integer i : sortedSeqLengths)
         {
-            genomeSize+=i;
+            genomeSize += i;
         }
-        
-        System.out.printf("Genome size = "+genomeSize);
-        System.out.println("\nNo. seqs = "+sortedSeqLengths.size());
-        System.out.println("Longest_contig\t"+sortedSeqLengths.get(0));
+
+        System.out.printf("Genome size = " + genomeSize);
+        System.out.println("\nNo. seqs = " + sortedSeqLengths.size());
+        System.out.println("Longest_contig\t" + sortedSeqLengths.get(0));
         double cumulativeSize = 0;
-        
+
         for (Integer i : sortedSeqLengths)
         {
-            cumulativeSize+=i;
-            if ((genomeSize/100)*90 < cumulativeSize && (genomeSize/100)*90 > (cumulativeSize - i))
+            cumulativeSize += i;
+
+            if ((genomeSize/100)*10 < cumulativeSize && (genomeSize/100)*10 > (cumulativeSize - i))
             {
-                System.out.println("N90\t"+i);
-            }
-            if ((genomeSize/100)*80 < cumulativeSize && (genomeSize/100)*80 > (cumulativeSize - i))
-            {
-                System.out.println("N80\t"+i);
-            }
-            if ((genomeSize/100)*70 < cumulativeSize && (genomeSize/100)*70 > (cumulativeSize - i))
-            {
-                System.out.println("N70\t"+i);
-            }
-            if ((genomeSize/100)*60 < cumulativeSize && (genomeSize/100)*60 > (cumulativeSize - i))
-            {
-                System.out.println("N60\t"+i);
-            }
-            if ((genomeSize/100)*50 < cumulativeSize && (genomeSize/100)*50 > (cumulativeSize - i))
-            {
-                System.out.println("N50\t"+i);
-            }
-            if ((genomeSize/100)*40 < cumulativeSize && (genomeSize/100)*40 > (cumulativeSize - i))
-            {
-                System.out.println("N40\t"+i);
-            }
-            if ((genomeSize/100)*30 < cumulativeSize && (genomeSize/100)*30 > (cumulativeSize - i))
-            {
-                System.out.println("N30\t"+i);
+                System.out.println("N10\t"+i);
             }
             if ((genomeSize/100)*20 < cumulativeSize && (genomeSize/100)*20 > (cumulativeSize - i))
             {
                 System.out.println("N20\t"+i);
             }
-            if ((genomeSize/100)*10 < cumulativeSize && (genomeSize/100)*10 > (cumulativeSize - i))
+            if ((genomeSize/100)*30 < cumulativeSize && (genomeSize/100)*30 > (cumulativeSize - i))
             {
-                System.out.println("N10\t"+i);
+                System.out.println("N30\t"+i);
+            }
+            if ((genomeSize/100)*40 < cumulativeSize && (genomeSize/100)*40 > (cumulativeSize - i))
+            {
+                System.out.println("N40\t"+i);
+            }
+            if ((genomeSize/100)*50 < cumulativeSize && (genomeSize/100)*50 > (cumulativeSize - i))
+            {
+                System.out.println("N50\t"+i);
+            }
+            if ((genomeSize/100)*60 < cumulativeSize && (genomeSize/100)*60 > (cumulativeSize - i))
+            {
+                System.out.println("N60\t"+i);
+            }
+            if ((genomeSize/100)*70 < cumulativeSize && (genomeSize/100)*70 > (cumulativeSize - i))
+            {
+                System.out.println("N70\t"+i);
+            }
+            if ((genomeSize/100)*80 < cumulativeSize && (genomeSize/100)*80 > (cumulativeSize - i))
+            {
+                System.out.println("N80\t"+i);
+            }
+            if ((genomeSize/100)*90 < cumulativeSize && (genomeSize/100)*90 > (cumulativeSize - i))
+            {
+                System.out.println("N90\t"+i);
             }
         }
 
-        
     }
-    
-    public double getGCContent(File fileName) throws FileNotFoundException, NoSuchElementException, BioException
+
+    public
+            double getGCContent(File fileName) throws FileNotFoundException, NoSuchElementException, BioException
     {
         // Set up sequence iterator
         double noSeqs = 0;
@@ -246,11 +265,11 @@ public class FastaFeatures
             }
             double currentGcCount = (gc * 100.0) / seq.length();
             //System.out.println(seq.getName() + ": "+ currentGcCount+ "%");
-            combinedGcTally+=currentGcCount;
+            combinedGcTally += currentGcCount;
             noSeqs++;
         }
-        double gcCount = (combinedGcTally/noSeqs)/100;
-        System.out.println("Overall gcCount = "+gcCount);
+        double gcCount = (combinedGcTally / noSeqs) / 100;
+        System.out.println("Overall gcCount = " + gcCount);
         return gcCount;
     }
 }
